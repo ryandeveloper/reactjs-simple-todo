7343600400128a6ceffda655499cc772822160ef
ryand <ryandumajil@gmail.com>
2020-11-01 23:53:44 +0800

    Updates

---
+++ precache-manifest.e3a0a8d2b0f43d160f971da9e10b8d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e854aa524686fc0cfc84fbf561d6d88",
+    "url": "/reactjs-simple-todo/index.html"
+  },
+  {
+    "revision": "ef2ada8f19539e400b0b",
+    "url": "/reactjs-simple-todo/static/css/main.0eab859c.chunk.css"
+  },
+  {
+    "revision": "a6d7452022f4ef34aac9",
+    "url": "/reactjs-simple-todo/static/js/2.b0cdd4e0.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/reactjs-simple-todo/static/js/2.b0cdd4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2ada8f19539e400b0b",
+    "url": "/reactjs-simple-todo/static/js/main.bc4f9d9b.chunk.js"
+  },
+  {
+    "revision": "d18e2698db576334e407",
+    "url": "/reactjs-simple-todo/static/js/runtime-main.7beb61f6.js"
+  }
+]);
